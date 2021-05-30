f6e132ffcd3cee49efcafee03f7fb3d2bb07531c
Michael Lan <michaellan202@gmail.com>
2021-05-30 10:20:32 -0700

    Rename template

--- iargs.lua
+++ iargs.lua
@@ -2,11 +2,10 @@ local queries = require "nvim-treesitter.query"
 
 local M = {}
 
--- TODO: In this function replace `module-template` with the actual name of your module.
 function M.init()
   require "nvim-treesitter".define_modules {
-    module_template = {
-      module_path = "module-template.internal",
+    iargs = {
+      module_path = "iargs.internal",
       is_supported = function(lang)
         -- TODO: you don't want your queries to be named `awesome-query`, do you ?
         return queries.get_query(lang, 'awesome-query') ~= nil
