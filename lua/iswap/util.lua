1ac8be32b92adbfb1371ab4f00963d9170574405
Michael Lan <michaellan202@gmail.com>
2021-05-30 22:20:57 -0700

    Remove unused utility function

--- lua/iswap/util.lua
+++ lua/iswap/util.lua
@@ -18,19 +18,6 @@ function M.within(a, b, c)
   return M.compare_position(a, b) and M.compare_position(b, c)
 end
 
--- Returns array of lines, where start and end_ are {row, col} where row is 1-indexed and col is 0-indexed
-function M.buf_get_range(bufnr, start, end_)
-  local lines = vim.api.nvim_buf_get_lines(bufnr, start[1], end_[1] + 1, false)
-  local n = #lines
-  if start[1] == end_[1] then
-    lines[1] = lines[1]:sub(start[2] + 1, end_[2])
-    return lines
-  end
-  lines[1] = lines[1]:sub(start[2] + 1, -1)
-  lines[n] = lines[n]:sub(1, end_[2])
-  return lines
-end
-
 function M.getchar_handler(on_err)
   local ok, key = pcall(vim.fn.getchar)
   if not ok then
