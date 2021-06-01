854809cd8014b09c882e55e74526116da9004c1e
Maximilian Schmidt <maximilian@schmidt.so>
2021-06-01 17:17:26 +0200

    Add additional nodes like array literals to rust queries

--- queries/rust/iswap-list.scm
+++ queries/rust/iswap-list.scm
@@ -1,5 +1,14 @@
 [
-    (parameters)
     (arguments)
+    (array_expression)
+    (declaration_list)
+    (enum_variant_list)
+    (field_declaration_list)
+    (meta_arguments)
+    (parameters)
+    (trait_bounds)
+    (tuple_expression)
+    (type_arguments)
+    (type_parameters)
 ] @iswap-list
 
