4f6dbd1a295d9b7dca9c941f2a7be2595376263d
Mathieu HATON <mathieu.haton@link-value.fr>
2022-02-06 16:38:33 +0100

    feat(rescript): add support

---
+++ queries/rescript/iswap-list.scm
@@ -0,0 +1,16 @@
+[
+  (arguments)
+  (array)
+  (array_pattern)
+  (formal_parameters)
+  (record)
+  (record_type)
+  (switch_expression)
+  (variant_type)
+  (variant_parameters)
+  (ternary_expression)
+  (tuple)
+] @iswap-list
+
+
+
