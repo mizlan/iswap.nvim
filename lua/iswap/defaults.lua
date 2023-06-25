b88e1423a975bb897af397f975b6df5ce8a683b5
Anshuman <amedhi@connect.ust.hk>
2023-06-25 23:22:01 +0800

    Optional, label parents to change the parent used

--- lua/iswap/defaults.lua
+++ lua/iswap/defaults.lua
@@ -3,6 +3,9 @@ local M = {}
 M.keys = 'asdfghjklqwertyuiopxcvbnm'
 M.hl_grey = 'Comment'
 M.hl_snipe = 'Search'
+M.hl_parent = 'Substitute'
+M.label_snipe_style = 'inline'
+M.label_parent_style = 'inline'
 M.hl_selection = 'Visual'
 M.flash_style = 'sequential'
 M.hl_flash = 'IncSearch'
@@ -13,5 +16,6 @@ M.shrink_key = ','
 M.autoswap = false
 M.only_current_line = true
 M.visual_select_list = true
+M.label_parents = true
 
 return M
